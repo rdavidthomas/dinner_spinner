c4f18ae70005285a297721751054cff4465b23dc
David Thomas <david.thomas@gtri.gatech.edu>
2016-12-17 14:45:16 -0500

    starting project

---
+++ main.c
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.c
+ * Author: david2
+ *
+ * Created on December 17, 2016, 2:40 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
